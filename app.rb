c9168a87f2d88c8c4b34421815dd79e42627aaf5
koyo <koyo@koyo.kr>
2020-11-14 19:54:25 +0000

    Print win-lose result

--- app.rb
+++ app.rb
@@ -39,7 +39,12 @@ class HangpersonApp < Sinatra::Base
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     letter = params[:guess].to_s[0]
-    @game.guess letter
+    begin
+      valid = @game.guess letter
+      flash[:message] = 'You have already used that letter.' unless valid
+    rescue ArgumentError
+      flash[:message] = 'Invalid guess.'
+    end
     redirect '/show'
   end
   
@@ -50,17 +55,38 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    case @game.check_win_or_lose
+    when :win
+      redirect '/win'
+    when :lose
+      redirect '/lose'
+    else
+      erb :show # You may change/remove this line
+    end
   end
   
   get '/win' do
     ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
+    case @game.check_win_or_lose
+    when :play
+      redirect '/show'
+    when :lose
+      redirect '/lose'
+    else
+      erb :win # You may change/remove this line
+    end
   end
   
   get '/lose' do
     ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
+    case @game.check_win_or_lose
+    when :win
+      redirect '/win'
+    when :play
+      redirect '/show'
+    else
+      erb :lose # You may change/remove this line
+    end
   end
   
 end
